6bc514c88913cb0e131620d395c23aa40e122992
dekunukem <dekunukem@gmail.com>
2022-01-08 14:36:48 +0000

    added notes about fixing gpu freq for stable UART baud rate

--- user_program/usb4vc_uart.py
+++ user_program/usb4vc_uart.py
@@ -7,4 +7,10 @@ import serial
 # import usb4vc_usb_scan
 # import usb4vc_ui
 
-print('hello world')
+ser = serial.Serial('/dev/serial0', 115200)
+
+while 1:
+    to_send = f"hello {int(time.time())}"
+    print(to_send)
+    ser.write(to_send.encode())
+    time.sleep(0.5)
