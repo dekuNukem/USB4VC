3e11062ecfb1a4db6e656df27cbc9d18048161b4
dekunukem <dekunukem@gmail.com>
2022-01-08 14:59:11 +0000

    working on UART commands

--- user_program/usb4vc_uart.py
+++ user_program/usb4vc_uart.py
@@ -2,15 +2,22 @@ import os
 import sys
 import time
 import serial
+import subprocess
 
 # import usb4vc_shared
 # import usb4vc_usb_scan
 # import usb4vc_ui
+# subprocess.getoutput(f"timeout 5 bluetoothctl --agent NoInputNoOutput paired-devices")
 
 ser = serial.Serial('/dev/serial0', 115200)
 
 while 1:
-    to_send = f"hello {int(time.time())}"
-    print(to_send)
-    ser.write(to_send.encode())
-    time.sleep(0.5)
+    received = ser.readline().decode().replace('\r', '')
+    if len(received) <= 0:
+        continue
+    print("I received:", received)
+    print(subprocess.getoutput(f"timeout 1 {received}"))
+    # to_send = f"hello {int(time.time())}"
+    # print(to_send)
+    # ser.write(to_send.encode())
+    # time.sleep(0.5)
