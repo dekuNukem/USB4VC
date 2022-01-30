3d6de0839b0e0c9fc5756f563d5343fb4cf3bb3c
dekunukem <dekunukem@gmail.com>
2022-01-30 15:21:11 +0000

    IBMPC 0.1.3 added XT key translation

--- firmware/ibmpc/Src/xt_kb.c
+++ firmware/ibmpc/Src/xt_kb.c
@@ -116,9 +116,55 @@ void xtkb_check_for_softreset(void)
   }
 }
 
+#define KEY_UP      103
+#define KEY_PAGEUP    104
+#define KEY_LEFT    105
+#define KEY_RIGHT   106
+#define KEY_END     107
+#define KEY_DOWN    108
+#define KEY_PAGEDOWN    109
+#define KEY_HOME    102
+#define KEY_INSERT    110
+#define KEY_DELETE    111
+
 // status 1 pressed 0 released
 uint8_t xtkb_press_key(uint8_t code, uint8_t status)
 {
+  // on XT keyboard those keys are on numpads, so need to translate over
+  switch(code)
+  {
+    case KEY_UP:
+      code = 72;
+      break;
+    case KEY_PAGEUP:
+      code = 74;
+      break;
+    case KEY_LEFT:
+      code = 75;
+      break;
+    case KEY_RIGHT:
+      code = 78;
+      break;
+    case KEY_END:
+      code = 79;
+      break;
+    case KEY_DOWN:
+      code = 80;
+      break;
+    case KEY_PAGEDOWN:
+      code = 81;
+      break;
+    case KEY_HOME:
+      code = 71;
+      break;
+    case KEY_INSERT:
+      code = 82;
+      break;
+    case KEY_DELETE:
+      code = 83;
+      break;
+  }
+
   if(code > 83) // not on XT keyboard
     return 0;
   if(status == 2) // typematic
