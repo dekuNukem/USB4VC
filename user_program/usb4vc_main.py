3755df691015a730af0be2697fabd986ae6fac3a
dekunukem <dekunukem@gmail.com>
2022-04-02 20:09:50 +0100

    updated clean OS setup notes, removed redundant wifi parsing

--- user_program/usb4vc_main.py
+++ user_program/usb4vc_main.py
@@ -42,7 +42,7 @@ def check_rpi_model():
     print('stored_model', stored_model)
     if current_model != stored_model:
         usb4vc_ui.oled_print_model_changed()
-        for x in range(50):
+        for x in range(20):
             print("!!!!!!!!!! DO NOT UNPLUG UNTIL I REBOOT !!!!!!!!!!")
             print("!!!!!!!!!! DO NOT UNPLUG UNTIL I REBOOT !!!!!!!!!!")
             print("!!!!!!!!!! DO NOT UNPLUG UNTIL I REBOOT !!!!!!!!!!")
@@ -56,53 +56,7 @@ def check_rpi_model():
         os.system('sudo reboot')
         time.sleep(10)
 
-def update_wifi_credentials():
-    config_file_path = '/etc/wpa_supplicant/wpa_supplicant.conf'
-    if os.path.isfile(config_file_path) is False:
-        print('update_wifi_credentials: system config file not found')
-        return
-    new_wifi_info_path = "/home/pi/usb4vc/config/wifi_info.json"
-    if os.path.isfile(new_wifi_info_path) is False:
-        print('update_wifi_credentials: user wifi info file not found')
-        return
-
-    with open(new_wifi_info_path) as json_file:
-        wifi_dict = json.load(json_file)
-
-    config_str = f"""
-ctrl_interface=DIR=/var/run/wpa_supplicant GROUP=netdev
-update_config=1
-country={wifi_dict['wifi_country_code']}
-
-network={{
-        ssid="{wifi_dict['wifi_name']}"
-        psk="{wifi_dict['wifi_password']}"
-}}
-"""
-
-    config_str_unsecured = f"""
-ctrl_interface=DIR=/var/run/wpa_supplicant GROUP=netdev
-update_config=1
-country={wifi_dict['wifi_country_code']}
-
-network={{
-        ssid="{wifi_dict['wifi_name']}"
-        key_mgmt=NONE
-}}
-"""
-    to_write = config_str
-    if len(wifi_dict['wifi_password']) == 0:
-        to_write = config_str_unsecured
-    with open(config_file_path, 'w') as wifi_config_file:
-        wifi_config_file.write(to_write)
-    os.system("wpa_cli -i wlan0 reconfigure")
-
 check_rpi_model()
-
-try:
-    update_wifi_credentials()
-except Exception as e:
-    print('update_wifi_credentials exception:', e)
 
 usb4vc_ui.ui_init()
 usb4vc_ui.ui_thread.start()
