a80bc80aff2724cbc1290858e09b659526e16bf4
dekunukem <dekunukem@gmail.com>
2022-04-02 20:03:07 +0100

    updated RPi OS setup note

--- user_program/sync.sh
+++ user_program/sync.sh
@@ -1,8 +1,8 @@
-# scp ./* pi@192.168.1.65:~/usb4vc/rpi_app
+# scp ./* pi@169.254.194.124:~/usb4vc/rpi_app
 
-# sh sync.sh; ssh -t pi@192.168.1.67 "pkill python3;cd ~/usb4vc/rpi_app;python3 usb4vc_main.py"
+# sh sync.sh; ssh -t pi@169.254.194.124 "pkill python3;cd ~/usb4vc/rpi_app;python3 usb4vc_main.py"
 
-scp ./* pi@192.168.1.62:~/usb4vc/rpi_app
-ssh -t pi@192.168.1.62 "pkill python3;cd ~/usb4vc/rpi_app;python3 usb4vc_main.py"
-# ssh -t pi@192.168.1.62 "pkill python3;cd ~/usb4vc/rpi_app;python3 firmware_flasher.py /home/pi/usb4vc/firmware/PBFW_IBMPC_PBID1_V0_1_5.hex"
-# ssh -t pi@192.168.1.62 "pkill python3;cd ~/usb4vc/rpi_app;python3 bb_tester.py"
+scp ./* pi@169.254.194.124:~/usb4vc/rpi_app
+# ssh -t pi@169.254.194.124 "pkill python3;cd ~/usb4vc/rpi_app;python3 usb4vc_main.py"
+# ssh -t pi@169.254.194.124 "pkill python3;cd ~/usb4vc/rpi_app;python3 firmware_flasher.py /home/pi/usb4vc/firmware/PBFW_IBMPC_PBID1_V0_1_5.hex"
+# ssh -t pi@169.254.194.124 "pkill python3;cd ~/usb4vc/rpi_app;python3 bb_tester.py"
