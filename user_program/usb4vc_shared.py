80c707923bbd1071b28b43205ed4343b7998f572
dekunukem <dekunukem@gmail.com>
2022-04-27 17:21:32 +0100

    0.1.1 rpi code release

--- user_program/usb4vc_shared.py
+++ user_program/usb4vc_shared.py
@@ -13,7 +13,7 @@ def ensure_dir(dir_path):
     if not os.path.exists(dir_path):
         os.makedirs(dir_path)
 
-RPI_APP_VERSION_TUPLE = (0, 1, 0)
+RPI_APP_VERSION_TUPLE = (0, 1, 1)
 
 code_name_to_value_lookup = {
 	'KEY_RESERVED':(0, 'kb_key'),
