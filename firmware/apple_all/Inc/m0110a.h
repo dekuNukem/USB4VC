96345a623e215f4d5011664c0a259551fabbeaa5
dekunukem <dekunukem@gmail.com>
2022-06-24 11:44:24 +0100

    resumed working on m0110a

---
+++ firmware/apple_all/Inc/m0110a.h
@@ -0,0 +1,17 @@
+#ifndef __M0110A_H
+#define __M0110A_H
+
+#ifdef __cplusplus
+ extern "C" {
+#endif 
+
+#include "stm32f0xx_hal.h"
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
+
+
---
+++ firmware/apple_all/Src/m0110a.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include "helpers.h"
+#include "shared.h"
+#include "m0110a.h"
