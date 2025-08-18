2adf1adf7003dd8f06e88712c0c14c40c1ce825b
dekuNukem <dekunukem@gmail.com>
2025-08-18 17:55:03 +0100

    updated setup instructions, dont compile xpadneo on RPi5

--- user_program/usb4vc_main.py
+++ user_program/usb4vc_main.py
@@ -40,7 +40,13 @@ def check_rpi_model():
     stored_model = get_stored_rpi_model()
     print('current_model', current_model)
     print('stored_model', stored_model)
-    if current_model != stored_model:
+    
+    if "Raspberry Pi 5".lower() in current_model.lower():
+        usb4vc_ui.oled_print_model_changed()
+        os.system("cd /home/pi/xpadneo/; sudo ./uninstall.sh")
+        save_rpi_model(current_model)
+        return
+    elif current_model != stored_model:
         usb4vc_ui.oled_print_model_changed()
         for x in range(20):
             print("!!!!!!!!!! DO NOT UNPLUG UNTIL I REBOOT !!!!!!!!!!")
@@ -63,7 +69,12 @@ try:
     boot_config = os.popen(f'cat {cmdline_path}').read()
     if "usbhid.mousepoll".lower() not in boot_config.lower():
         print(f"@@@@@@@@@@@@ writing usbhid.mousepoll to {cmdline_path} @@@@@@@@@@@@")
-        new_config = boot_config.replace('\r', '').replace('\n', '').strip() + ' usbhid.mousepoll=0\n'
+        new_config = boot_config.replace('\r', '').replace('\n', '').strip() + ' usbhid.mousepoll=4\n'
+        with open(cmdline_path, 'w') as ffff:
+            ffff.write(new_config)
+    elif "usbhid.mousepoll=0" in boot_config:
+        print(f"@@@@@@@@@@@@ writing usbhid.mousepoll to {cmdline_path} @@@@@@@@@@@@")
+        new_config = boot_config.replace('\r', '').replace('\n', '').strip().replace("usbhid.mousepoll=0", "usbhid.mousepoll=4")
         with open(cmdline_path, 'w') as ffff:
             ffff.write(new_config)
 except Exception as e:
