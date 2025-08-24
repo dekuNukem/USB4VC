9e96fd7cb16e6df103256fa742a6f40bb5fc685d
dekuNukem <dekunukem@gmail.com>
2025-08-24 22:42:02 +0100

    restored stable code, current work in new branch

--- user_program/usb4vc_main.py
+++ user_program/usb4vc_main.py
@@ -7,8 +7,6 @@ import usb4vc_usb_scan
 import usb4vc_ui
 import subprocess 
 
-# usb4vc_ui.reset_pboard()
-
 def get_current_rpi_model():
     current_model = 'Unknown'
     try:
@@ -40,13 +38,7 @@ def check_rpi_model():
     stored_model = get_stored_rpi_model()
     print('current_model', current_model)
     print('stored_model', stored_model)
-    
-    if "Raspberry Pi 5".lower() in current_model.lower():
-        usb4vc_ui.oled_print_model_changed()
-        os.system("cd /home/pi/xpadneo/; sudo ./uninstall.sh")
-        save_rpi_model(current_model)
-        return
-    elif current_model != stored_model:
+    if current_model != stored_model:
         usb4vc_ui.oled_print_model_changed()
         for x in range(20):
             print("!!!!!!!!!! DO NOT UNPLUG UNTIL I REBOOT !!!!!!!!!!")
@@ -64,18 +56,12 @@ def check_rpi_model():
 
 check_rpi_model()
 
-cmdline_path = "/boot/firmware/cmdline.txt"
 try:
-    boot_config = os.popen(f'cat {cmdline_path}').read()
+    boot_config = os.popen('cat /boot/cmdline.txt').read()
     if "usbhid.mousepoll".lower() not in boot_config.lower():
-        print(f"@@@@@@@@@@@@ writing usbhid.mousepoll to {cmdline_path} @@@@@@@@@@@@")
+        print("@@@@@@@@@@@@ writing usbhid.mousepoll to /boot/cmdline.txt @@@@@@@@@@@@")
         new_config = boot_config.replace('\r', '').replace('\n', '').strip() + ' usbhid.mousepoll=8\n'
-        with open(cmdline_path, 'w') as ffff:
-            ffff.write(new_config)
-    elif "usbhid.mousepoll=0" in boot_config:
-        print(f"@@@@@@@@@@@@ writing usbhid.mousepoll to {cmdline_path} @@@@@@@@@@@@")
-        new_config = boot_config.replace('\r', '').replace('\n', '').strip().replace("usbhid.mousepoll=0", "usbhid.mousepoll=8")
-        with open(cmdline_path, 'w') as ffff:
+        with open('/boot/cmdline.txt', 'w') as ffff:
             ffff.write(new_config)
 except Exception as e:
     print('usbhid.mousepoll exception:', e)
@@ -84,17 +70,16 @@ usb4vc_ui.ui_init()
 usb4vc_ui.ui_thread.start()
 
 usb4vc_usb_scan.usb_device_scan_thread.start()
-# usb4vc_usb_scan.raw_input_event_parser_thread.start()
+usb4vc_usb_scan.raw_input_event_parser_thread.start()
 
 while 1:
     time.sleep(2)
-    if os.path.exists("/sys/module/bluetooth/parameters/disable_ertm"):
-        try:
-            ertm_status = subprocess.getoutput("cat /sys/module/bluetooth/parameters/disable_ertm").replace('\n', '').replace('\r', '').strip()
-            if ertm_status != 'Y':
-                # print('ertm_status:', ertm_status)
-                # print("Disabling ERTM....")
-                subprocess.call('echo Y > /sys/module/bluetooth/parameters/disable_ertm')
-                # print("DONE")
-        except Exception:
-            continue
+    try:
+        ertm_status = subprocess.getoutput("cat /sys/module/bluetooth/parameters/disable_ertm").replace('\n', '').replace('\r', '').strip()
+        if ertm_status != 'Y':
+            print('ertm_status:', ertm_status)
+            print("Disabling ERTM....")
+            subprocess.call('echo 1 > /sys/module/bluetooth/parameters/disable_ertm')
+            print("DONE")
+    except Exception:
+        continue
